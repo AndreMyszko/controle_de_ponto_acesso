9aa591d35ed0474cdf3e7673268547f6c984ffba
ANDRÉ MYSZKO <andre.myszko@gmail.com>
2021-07-27 15:31:28 -0300

    first commit

---
+++ src/main/java/projeto/controle_de_ponto_acesso/ControleDePontoAcessoApplication.java
@@ -0,0 +1,13 @@
+package projeto.controle_de_ponto_acesso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDePontoAcessoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDePontoAcessoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projeto/controle_de_ponto_acesso/ControleDePontoAcessoApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto.controle_de_ponto_acesso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDePontoAcessoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
